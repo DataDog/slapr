d798fa03db6c4730e038d60ee25712bfc22570fd
Florimond Manca <florimond.manca@datadoghq.com>
2020-02-06 16:35:54 +0100

    Lower review states

--- action/github.py
+++ action/github.py
@@ -16,4 +16,4 @@ def read_event() -> dict:
 def get_pr_review_states(pr_number: int) -> List[str]:
     repo = settings.GITHUB_REPO
     reviews = gh.get_repo(repo).get_pull(pr_number).get_reviews()
-    return [review.state for review in reviews]
+    return [review.state.lower() for review in reviews]
