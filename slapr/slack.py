605d821c5f1c5fa607611e8d9242ed156a3200f5
Alexandre Yang <alexandre.yang@datadoghq.com>
2020-02-07 13:42:46 +0100

    Add debug messages for find_timestamp_of_review_requested_message

--- slapr/slack.py
+++ slapr/slack.py
@@ -21,7 +21,10 @@ def find_timestamp_of_review_requested_message(pr_url: str, channel_id: str) -> 
 
     for message in filter(lambda m: m["type"] == "message", messages):
         text = message.get("text", "")
+        print("Text to match: {}".format(text))
+        print("settings.SLAPR_SEARCH_PATTERN: {}".format(settings.SLAPR_SEARCH_PATTERN))
         match = re.match(settings.SLAPR_SEARCH_PATTERN, text)
+        print("match: {}".format(match))
 
         if match is None:
             continue
