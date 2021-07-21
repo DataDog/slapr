8f1ec1cb099adbf3750239e1fc3f8815c5868f6a
Taylor Brown <taylor.brown@banno.com>
2021-07-21 18:00:39 -0500

    Modify emojis used

--- slapr/__main__.py
+++ slapr/__main__.py
@@ -20,10 +20,10 @@ config = Config(
     slack_channel_id=os.environ["SLACK_CHANNEL_ID"],
     slapr_bot_user_id=os.environ["SLAPR_BOT_USER_ID"],
     emoji_review_started=os.environ.get("SLAPR_EMOJI_REVIEW_STARTED", "review_started"),
-    emoji_approved=os.environ.get("SLAPR_EMOJI_APPROVED", "approved"),
+    emoji_approved=os.environ.get("SLAPR_EMOJI_APPROVED", "tests-passed"),
     emoji_needs_change=os.environ.get("SLAPR_EMOJI_CHANGES_REQUESTED", "changes_requested"),
-    emoji_merged=os.environ.get("SLAPR_EMOJI_MERGED", "merged"),
-    emoji_closed=os.environ.get("SLAPR_EMOJI_CLOSED", "closed"),
+    emoji_merged=os.environ.get("SLAPR_EMOJI_MERGED", "merged-purple"),
+    emoji_closed=os.environ.get("SLAPR_EMOJI_CLOSED", "rip"),
 )
 
 main(config)
